2f136219d75a8e98ce62ea04dc2a7264009dfff7
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-11-28 12:48:21 +0000

    document similarity added

--- clustering.py
+++ clustering.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pickle
 import random
 
+# Question 1
 def plot(collection):
 	for i in range(1):
 		document = random.choice(collection)
@@ -12,13 +13,35 @@ def plot(collection):
 		plt.hist(document, bins=len(document))
 		plt.show()
 
-
-
- # Q 1 (5 marks). For each of the 3 collections, 
- # sample 5 documents and 5 words at random and plot each 
+ # Q 1 (5 marks). For each of the 3 collections,
+ # sample 5 documents and 5 words at random and plot each
  # T -dimensional vector (the distributions) as a bar chart.
 
-file = open('C1', 'rb')
-C1 = pickle.load(file)
-file.close()
+collections = []
+print("Loading collections...")
+for c in ['C1', 'C2', 'C3']:
+	file = open(c, 'rb')
+	collections.append(pickle.load(file))
+	file.close()
 
+
+# Question 3
+def cosineSimilarity(x, y):
+	sum=0
+	for i in range(len(x)):
+		sum+=x[i]*y[i]
+	return sum/(len(x)*len(y))
+
+def averageSimilarity(collection):
+	sim=0
+	pairs = []
+	for i in range(100):
+		pairs.append([random.choice(collection), random.choice(collection)])
+
+	for pair in pairs:
+		sim += cosineSimilarity(pair[0], pair[1])
+	return sim/100
+
+print("Average document similarity of C1:", averageSimilarity(collections[0]))
+print("Average document similarity of C2:", averageSimilarity(collections[1]))
+print("Average document similarity of C3:", averageSimilarity(collections[2]))
